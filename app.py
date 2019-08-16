4b046b63ca865df7e294219fc1439ace3dde39ff
Reecha Soni <43318992+reechasoni@users.noreply.github.com>
2019-08-16 23:56:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ redis = Redis(host=redis_host, port=6379)
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return '<h1 style="color:blue">Welcome to Java Home cloud python app - count the number of hits to redis -> {}</h1>'.format(count)
+    return '<h1 style="color:orange">Welcome to new Java Home cloud python app - count the number of hits to redis -> {}</h1>'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
